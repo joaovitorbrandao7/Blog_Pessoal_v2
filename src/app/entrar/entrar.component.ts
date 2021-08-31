54c3220a481af7776a1bfc3e0be2ed3f281c13d8
joaovitorbrandao7 <joaovitor.brandao21@outlook.com>
2021-08-31 14:15:49 -0300

    Consumo API - Login e Cadastro

---
+++ src/app/entrar/entrar.component.ts
@@ -0,0 +1,45 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../service/auth.service';
+import { UserLogin } from './../model/UserLogin';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-entrar',
+  templateUrl: './entrar.component.html',
+  styleUrls: ['./entrar.component.css']
+})
+export class EntrarComponent implements OnInit {
+
+  userLogin: UserLogin = new UserLogin()
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
+
+      this.userLogin = resp
+      environment.token = this.userLogin.token
+      environment.nome = this.userLogin.nome
+      environment.foto = this.userLogin.foto
+      environment.id = this.userLogin.id
+  
+      this.userLogin.foto
+
+      alert('Logado com sucesso!')
+      this.router.navigate(['/inicio'])
+    }, erro =>{
+      if(erro.status == 500){
+        alert('Usuário ou senha estão incorretos!')
+      }
+    })
+  }
+
+}
