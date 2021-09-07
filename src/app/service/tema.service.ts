206976ac6f7fbb13f436da6866e214b6379351ab
joaovitorbrandao7 <joaovitor.brandao21@outlook.com>
2021-09-07 19:17:03 -0300

    Implantação do put e delete de temas

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -15,6 +15,10 @@ export class TemaService {
       headers: new HttpHeaders().set('Authorization', environment.token)
     }
 
+    getByIdTema(id: number): Observable<Tema>{
+      return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+    }
+
     getAllTema(): Observable<Tema[]>{
       return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
     } 
@@ -22,5 +26,11 @@ export class TemaService {
     postTema(tema: Tema): Observable<Tema>{
       return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
     }
-     
+
+    putTema(tema: Tema): Observable<Tema>{
+      return this.http.put<Tema>('http://localhost:8080/tema', tema, this.token)
+    }
+     deleteTema(id: number){
+       return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+     }
 }
