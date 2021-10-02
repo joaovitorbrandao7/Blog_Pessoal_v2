95cebba4f1f6fa8195d9117c1ddf1460f70c086d
joaovitorbrandao7 <joaovitor.brandao21@outlook.com>
2021-10-02 12:49:31 -0300

    Implementação do GET e POST de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,5 +1,5 @@
 import { UserLogin } from './../model/UserLogin';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
@@ -10,16 +10,21 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class AuthService {
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
+    token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://blogpessoaljv.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('https://blogpessoaljv.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogpessoaljv.herokuapp.com/usuarios/${id}`, this.token)
   }
 
    logado(){
