36578182d95b9f60d36d1353453a42bba8bd90e5
joaovitorbrandao7 <joaovitor.brandao21@outlook.com>
2021-10-10 19:20:55 -0300

    Deploy do blog

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -18,7 +18,19 @@ export class PostagemService {
       return this.http.get<Postagem[]>('https://blogpessoaljv.herokuapp.com/postagens', this.token)
     }
 
+    getByIdPostagem(id: number): Observable<Postagem>{
+      return this.http.get<Postagem>(`https://blogpessoaljv.herokuapp.com/postagens/${id}`, this.token)
+    }
+
     postPostagem(postagem: Postagem) : Observable<Postagem>{
       return this.http.post<Postagem>('https://blogpessoaljv.herokuapp.com/postagens', postagem, this.token)
+    }
+
+    putPostagem(postagem: Postagem): Observable<Postagem>{
+      return this.http.put<Postagem>('https://blogpessoaljv.herokuapp.com/postagens', postagem, this.token)
+    }
+
+    deletePostagem(id: number){
+      return this.http.delete<Postagem>(`https://blogpessoaljv.herokuapp.com/postagens/${id}`, this.token)
     }
 }
